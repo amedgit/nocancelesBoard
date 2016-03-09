3838d52e2ddd0fa526ba69bed5f97731b20a794a
amed <www.hamed.it@gmail.com>
2016-03-09 11:01:43 +0100

    models like fixed

--- app/models/ocio.rb
+++ app/models/ocio.rb
@@ -4,5 +4,5 @@ class Ocio < ActiveRecord::Base
   has_one :pic , as: :imageable , dependent: :destroy
   has_many :comments , as: :commentable , dependent: :destroy
   accepts_nested_attributes_for :pic , allow_destroy: true
-  scope :like, ->(args) { where("city like '%#{args}%' OR cat like '%#{args}%' OR title like '%#{args}%' OR created_at like '%#{args}%'")}
+  scope :like, ->(args) { where("city like '%#{args}%' OR cat like '%#{args}%' OR title like '%#{args}%'  ")}
 end
